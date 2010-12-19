5bf3eaeeeff73d6cb578e0eb223605be72b1258a
Sean McGivern <sean@mcgivern.me.uk>
2010-12-19 21:01:43 +0000

    URMs now run correctly

--- ext/urm-evaluator.js
+++ ext/urm-evaluator.js
@@ -1,36 +1,74 @@
-function zero(registers, n) {
-	return replaceIndex(registers, n, 0);
+var stepLimit = 1000;
+
+var z = replaceIndex(zero);
+var s = replaceIndex(successor);
+var t = replaceIndex(transfer);
+
+function j(registers, next, m, n, p) {
+    return [registers, registers[m - 1] == registers[n - 1] ? p : next];
 }
 
-function successor(registers,, n) {
-	return replaceIndex(registers, n, orZero(registers, n - 1) + 1);
+function zero(registers, n) { return 0; }
+function successor(registers, n) { return orZero(registers, n - 1) + 1; }
+function transfer(registers, n, m) { return orZero(registers, m - 1); }
+function orZero(arr, i) { return parseInt('0' + arr[i], 10); }
+
+function replaceIndex(func) {
+    return function(registers, next, n, m) {
+        var newRegisters = registers.slice(0);
+        newRegisters[n - 1] = func(registers, n, m);
+        return [newRegisters, next];
+    };
 }
 
-function transfer(registers, n, m) {
-	return replaceIndex(registers, n, orZero(registers, m - 1))];
+function runURM(program, registers, i, log) {
+    function newLog(x) { return log.concat([[x].concat(registers)]); }
+
+    if (log.length >= stepLimit) {
+        return log.concat([['STOP',
+                            'No end after ' + stepLimit + ' steps']]);
+    }
+
+    if (i > program.length) { return newLog('STOP'); }
+
+    var args = [registers, i + 1].concat(program[i - 1][1]);
+    var result = program[i - 1][0].apply(this, args);
+
+    return runURM(program, result[0], result[1], newLog(i));
 }
 
-function jump(registers, m, n, p, q) {
-}
+function fillZeroes(lines) {
+    function orZeroMap(length) {
+        return function(array) {
+            var newArray = array.slice(0);
+            for (i = 0; i < length; i++) {
+                if (typeof newArray[i] != 'string') {
+                    newArray[i] = orZero(newArray, i);
+                }
+            }
+            return [newArray];
+        };
+    }
 
-function orZero(arr, i) {
-	return parseInt('0' + arr[i], 10);
-}
+    var lengths = $.map(lines, function(v, i) { return v.length; });
 
-function replaceIndex(registers, n, m) {
-	var newRegisters = registers.slice(0);
-	newRegisters[n - 1] = m;
-	return newRegisters;
+    return $.map(lines, orZeroMap(Math.max.apply(Math, lengths)));
 }
 
 function texFormat(lines) {
-	var lineTexFormat = function(line, i) {
-		return $.
-			map(line, function(c, j) {
-				return (typeof c == 'string' ?
-						'\\text{' + c + '}' : '' + c)
-			}).join(' & ');
-	}
+    function lineTexFormat(line, i) {
+        return $.
+            map(line, function(c, j) {
+                return (typeof c == 'string' ?
+                        '\\text{' + c + '}' : '' + c)
+            }).join(' & ');
+    }
 
-	return $.map(lines, lineTexFormat).join(' \\\\\n');
+    return $.map(fillZeroes(lines), lineTexFormat).join(' \\\\\n');
+}
+
+function plainTextFormat(lines) {
+    function linePlainTextFormat(l) { return l.join('\t'); }
+
+    return $.map(fillZeroes(lines), linePlainTextFormat).join('\n');
 }
--- test/test.js
+++ test/test.js
@@ -5,8 +5,12 @@ function arrayEqual(x, y) {
     var a = x.sort();
     var b = y.sort();
 
-    for (var i = 0; b[i]; i++) {
-        if (a[i].constructor == Array && b[i].constructor == Array) {
+    for (var i = 0; i < a.length; i++) {
+        if (typeof a[i] == 'undefined' && typeof b[i] == 'undefined') {
+            continue;
+        } else if (typeof a[i] == 'undefined' || typeof b[i] == 'undefined') {
+            return false;
+        } else if (a[i].constructor == Array && b[i].constructor == Array) {
             if (!arrayEqual(a[i], b[i])) { return false ;}
         } else {
             if (a[i] !== b[i]) { return false; }
@@ -23,27 +27,80 @@ new Test.Unit.Runner({
     teardown: function() {
     },
 
-	testZero: function() { with(this) {
-		assert(arrayEqual(zero([2, 3, 4], 1), [0, 3, 4]));
-		assert(arrayEqual(zero([2, 3, 4], 5), [2, 3, 4, , 0]));
-	}},
+    testZero: function() { with(this) {
+        assert(arrayEqual(z([2, 3, 4], 1, 1), [[0, 3, 4], 1]));
+        assert(arrayEqual(z([2, 3, 4], 2, 5), [[2, 3, 4, , 0], 2]));
+    }},
 
-	testSuccessor: function() { with(this) {
-		assert(arrayEqual(successor([2, 3, 4], 1), [3, 3, 4]));
-		assert(arrayEqual(successor([2, 3, 4], 5), [2, 3, 4, , 1]));
-	}},
+    testSuccessor: function() { with(this) {
+        assert(arrayEqual(s([2, 3, 4], 3, 1), [[3, 3, 4], 3]));
+        assert(arrayEqual(s([2, 3, 4], 4, 5), [[2, 3, 4, , 1], 4]));
+    }},
 
-	testTransfer: function() { with(this) {
-		assert(arrayEqual(transfer([2, 3, 4], 2, 1), [2, 2, 4]));
-		assert(arrayEqual(transfer([2, 3, 4], 2, 5), [2, 0, 4]));
-	}},
+    testTransfer: function() { with(this) {
+        assert(arrayEqual(t([2, 3, 4], 5, 2, 1), [[2, 2, 4], 5]));
+        assert(arrayEqual(t([2, 3, 4], 6, 2, 5), [[2, 0, 4], 6]));
+    }},
 
-	testJump: function() { with(this) {
-	}},
+    testJump: function() { with(this) {
+        assert(arrayEqual(j([2, 2, 4], 1, 1, 2, 5), [[2, 2, 4], 5]));
+        assert(arrayEqual(j([2, 2, 4], 1, 1, 3, 5), [[2, 2, 4], 1]));
+    }},
 
-	testTexFormat: function() { with(this) {
-		assert(texFormat([['STOP']]) == '\\text{STOP}');
-		assert(texFormat([[1, 2], [3]]) == '1 & 2 \\\\\n3');
-		assert(texFormat([['a'], [2, 'c']]) == '\\text{a} \\\\\n2 & \\text{c}');
-	}},
+    // http://planetmath.org/?op=getobj&from=objects&id=11933
+    testRunURM: function() { with(this) {
+        var additionProgram = [
+            [j, [2, 3, 5]],
+            [s, [1]],
+            [s, [3]],
+            [j, [1, 1, 1]],
+            [z, [3]]
+        ];
+
+        var transferProgram = [
+            [j, [3, 4, 6]],
+            [z, [4]],
+            [s, [4]],
+            [j, [3, 4, 6]],
+            [j, [3, 3, 3,]]
+        ];
+
+        var infiniteProgram = [[j, [1, 1, 1]]];
+
+        assert(arrayEqual(runURM(additionProgram, [3, 2], 1, []).pop(),
+                          ['STOP', 5, 2, 0]));
+
+        assert(arrayEqual(runURM(additionProgram, [4, 5, 0, 0], 1, []).pop(),
+                          ['STOP', 9, 5, 0, 0]));
+
+        assert(arrayEqual(runURM(transferProgram, [0, 0, 9, 7], 1, []).pop(),
+                          ['STOP', 0, 0, 9, 9]));
+
+        assert(arrayEqual(runURM(infiniteProgram, [0], 1, []).pop(),
+                          ['STOP', 'No end after 1000 steps']));
+    }},
+
+    testFillZeroes: function() { with(this) {
+        var sparseArray = [1];
+        sparseArray[3] = 1;
+
+        assert(arrayEqual(fillZeroes([[1], [2, 3]]), [[1, 0], [2, 3]]));
+
+        assert(arrayEqual(fillZeroes([sparseArray, [1, 2]]),
+                          [[1, 0, 0, 1], [1, 2, 0, 0]]));
+    }},
+
+    testTexFormat: function() { with(this) {
+        assert(texFormat([['STOP']]) == '\\text{STOP}');
+        assert(texFormat([[1, 2], [3]]) == '1 & 2 \\\\\n3 & 0');
+
+        assert(texFormat([['a'], [2, 'c']]) ==
+               '\\text{a} & 0 \\\\\n2 & \\text{c}');
+    }},
+
+    testPlainTextFormat: function() { with(this) {
+        assert(plainTextFormat([['STOP']]) == 'STOP');
+        assert(plainTextFormat([[1, 2], [3]]) == '1\t2\n3\t0');
+        assert(plainTextFormat([['a'], [2, 'c']]) == 'a\t0\n2\tc');
+    }},
 });
